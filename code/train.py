7fd761422e91be30055a7e182ba5ee208c7ec527
Tymoteusz Kwiecinski <tkk2002tkk@gmail.com>
2024-05-19 10:29:45 +0000

    more often snapshots of checkpoints

--- code/train.py
+++ code/train.py
@@ -1,3 +1,7 @@
+
+import sys
+sys.path.append("..")
+
 from src.datasets import TileTripletsDataset, GetBands, RandomFlipAndRotate, ClipAndScale, ToFloatTensor, triplet_dataloader
 from src.tilenet import make_tilenet
 from src.training import prep_triplets, train_triplet_epoch
@@ -10,20 +14,22 @@ import os
 import torch
 from time import time
 
+from pathlib import Path
+
 # script to train the tile2vec model
 
 
 # values to change during training
-model_name = 'TileNet_tile_30.ckpt'
+model_name = 'TileNet_128_dims.ckpt'
 img_type = "landsat" # images are in float - this parameter specifies that there is a need for normalization of floats
-tile_dir = '/storage/tile2vec/tiles_very_small' # directory where are the triplets stored
+tile_dir = Path('/storage/tile2vec/tiles') # directory where are the triplets stored
 bands = 13
 augment = True
 batch_size = 50
 shuffle = True
 num_workers = 16
 n_triplets = 50000
-z_dim = 512
+z_dim = 128
 
 
 # initialize GPU
@@ -40,7 +46,7 @@ dataloader = triplet_dataloader(img_type, tile_dir, bands=bands, augment=augment
 print('Dataloader set up complete.')
 
 
-
+torch.autograd.set_detect_anomaly(True)
 # function to initialize model structure
 TileNet = make_tilenet(in_channels=bands, z_dim=z_dim)
 TileNet.train()
@@ -54,37 +60,41 @@ optimizer = optim.Adam(TileNet.parameters(), lr=lr, betas=(0.5, 0.999))
 
 
 # training-level parameters 
-epochs = 50
+epochs = 200
 margin = 10
 l2 = 0.01
 print_every = 1000 # how often model will produce the information about the loss
 save_models = True
 
 # create model directory
-model_dir = '/storage/tile2vec/models'
+model_dir = Path('/storage/tile2vec/models')
 if not os.path.exists(model_dir): 
     os.makedirs(model_dir)
     
 
-results_fn = "/storage/tile2vec/results_fn"
+results_path = Path("/storage/tile2vec/results")
 
-# avg_losses = []
-# avg_l_ns = []
-# avg_l_ds = []
-# avg_l_nds = []
+avg_losses = []
+avg_l_ns = []
+avg_l_ds = []
+avg_l_as = []
 
 t0 = time()
-with open(results_fn, 'w') as file:
-    print('Begin training.................')
-    for epoch in range(0, epochs):
-        (avg_loss, avg_l_n, avg_l_d, avg_l_nd) = train_triplet_epoch(
+print('Begin training.................')
+for epoch in range(0, epochs):
+        (avg_loss, avg_l_n, avg_l_d, avg_l_a) = train_triplet_epoch(
             TileNet, cuda, dataloader, optimizer, epoch+1, margin=margin, l2=l2,
             print_every=print_every, t0=t0)
         
-        # avg_losses.append(avg_loss)
-        # avg_l_ns.append(avg_l_n)
-        # avg_l_ds.append(avg_l_d)
-        # avg_l_nds.append(avg_l_nd)
+        if save_models and epochs % 50:
+            print("saving model")
+            model_fn = os.path.join(model_dir, str(model_name, "epoch_", epoch, ".ckpt"))
+            torch.save(TileNet.state_dict(), model_fn)
+        
+        avg_losses.append(avg_loss)
+        avg_l_ns.append(avg_l_n)
+        avg_l_ds.append(avg_l_d)
+        avg_l_as.append(avg_l_a)
 
 # Save model after last epoch
 if save_models:
@@ -92,6 +102,6 @@ if save_models:
     model_fn = os.path.join(model_dir, model_name)
     torch.save(TileNet.state_dict(), model_fn)
  
-# with open(model_name + ".pkl", "wb") as f:
-#     avg = {"losses": avg_losses, "l_n": avg_l_ns, "l_d": avg_l_ds, "l_nd": avg_l_nds}
-#     pickle.dump(avg, f)
+with open(results_path / str(model_name + ".pkl"), "wb") as f:
+    avg = {"losses": avg_losses, "l_n": avg_l_ns, "l_d": avg_l_ds, "l_a": avg_l_as}
+    pickle.dump(avg, f)
